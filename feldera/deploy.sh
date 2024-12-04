cb94421b2083d5b73c3b0d36e1966ab56a230df7
Gerd Zellweger <mail@gerdzellweger.com>
2024-12-03 22:22:02 -0800

    Add gh-action for deploying feldera.
    Signed-off-by: Gerd Zellweger <mail@gerdzellweger.com>

--- feldera/deploy.sh
+++ feldera/deploy.sh
@@ -1,4 +1,5 @@
 fda create xls || true
 fda stop xls || true
 fda program set xls program.sql --udf-rs udf/src/lib.rs --udf-toml udf/udf.toml
+fda set-config xls workers 8
 fda restart --recompile xls
