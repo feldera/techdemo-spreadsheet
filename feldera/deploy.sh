ce04cf7331bc3d13d71ad2e7355fe25d2e87b316
Gerd Zellweger <mail@gerdzellweger.com>
2025-06-15 00:08:33 -0700

    Enable fault tolerance in deploy.sh

--- feldera/deploy.sh
+++ feldera/deploy.sh
@@ -2,4 +2,7 @@ fda create xls || true
 fda stop xls || true
 fda program set xls program.sql --udf-rs udf/src/lib.rs --udf-toml udf/udf.toml
 fda set-config xls workers 8
+fda set-config xls storage true
+fda set-config xls fault_tolerance at_least_once
+fda set-config xls checkpoint_interval 21600
 fda restart --recompile xls
